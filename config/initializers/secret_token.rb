be37787792e6149d9fd2f8835698d1e08535c6ec
Capt. Kello <captainkello@gmail.com>
2011-01-01 08:31:37 -1000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ahi2::Application.config.secret_token = '2964bb3fa57e7cf0933a984fb4f323136f852aa31cb3090ec66021f7a709b9a42dc5e557a700d56a75a0ea5dd1966067ae696fc7b1b247808cef3c58ed75ab3e'
